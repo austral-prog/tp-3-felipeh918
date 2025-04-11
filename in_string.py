5b5bc9bcbfd451ffb8597c01068fdcf0b12dc482
felipeh918 <fhouriet@mail.austral.edu.ar>
2025-04-11 11:26:11 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,5 @@
 def check_vowels():
-    nombre = input("ingrese nombre: ").lower()
+    	nombre = input("ingrese nombre: ").lower()
 	print(f"contiene a: {"a" in nombre}")
 	print(f"contiene e: {"e" in nombre}")
 	print(f"contiene i: {"i" in nombre}")
